434fd6128b6d01f15831e9ed17ff84aad3d5ea43
Daniel Kapulkin <dkapulkin@zushealth.com>
2022-04-07 12:39:42 -0500

    practice math

--- Learning Go/practice/main.go
+++ Learning Go/practice/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"math"
 )
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	input, _ := reader.ReadString('\n') // could return an error, which we're ignoring with _ assignment
-	fmt.Println("You entered: ", input)
+	i1, i2, i3 := 12, 42, 68
+	intSum := i1 + i2 + i3
+	fmt.Println(intSum)
 
-	fmt.Print("Enter a number: ")
-	numInput, _ := reader.ReadString('\n')
-	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Value of number: ", aFloat)
-	}
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3 // binary precision issue will produce 164.899999
+	// floatSum = math.Round(floatSum) // no colon operator because var already exists, returns an int
+	floatSum = math.Round(floatSum*100) / 100 // gives 2 decimal point precision
+	fmt.Println(floatSum)
+
+	circleRadius := 15.5
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("Circumference: %.2f\n", circumference)
 }

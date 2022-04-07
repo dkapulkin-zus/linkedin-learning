7ae0e9c5dcedcd73979675fca6358caac179c0af
Daniel Kapulkin <dkapulkin@zushealth.com>
2022-04-07 12:32:30 -0500

    using bufio

--- Learning Go/practice/main.go
+++ Learning Go/practice/main.go
@@ -1,35 +1,26 @@
 package main
 
-import "fmt"
-
-const cInt int = 64
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 
-	// explicit typing
-	var aString string = "This is Go!"
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n') // could return an error, which we're ignoring with _ assignment
+	fmt.Println("You entered: ", input)
 
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
-
-	var anInt int = 42
-	fmt.Println(anInt)
-	fmt.Printf("The variable's type is %T\n", anInt)
-
-	// explicit typing w/o init value
-	var defInt int
-	fmt.Println(defInt)
-	fmt.Printf("The variable's type is %T\n", defInt)
-
-	// implicit typing
-	var anotherString = "This is still Go!"
-	fmt.Println(anotherString)
-	fmt.Printf("The variable's type is %T\n", anotherString)
-
-	// implicit typing, avoid var syntax
-	myString := "Alternative assignment syntax" // this syntax only works for vars inside functions
-	fmt.Println(myString)
-	fmt.Printf("The variable's type is %T\n", myString)
-
-	fmt.Println(cInt)
+	fmt.Print("Enter a number: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number: ", aFloat)
+	}
 }

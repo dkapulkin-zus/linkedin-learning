43e217f50cdf3369e47ec3e099f7c9a445311f6c
Daniel Kapulkin <dkapulkin@zushealth.com>
2022-04-07 11:59:14 -0500

    creating variables

--- Learning Go/practice/main.go
+++ Learning Go/practice/main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+const cInt int = 64
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	// explicit typing
+	var aString string = "This is Go!"
+
+	fmt.Println(aString)
+	fmt.Printf("The variable's type is %T\n", aString)
+
+	var anInt int = 42
+	fmt.Println(anInt)
+	fmt.Printf("The variable's type is %T\n", anInt)
+
+	// explicit typing w/o init value
+	var defInt int
+	fmt.Println(defInt)
+	fmt.Printf("The variable's type is %T\n", defInt)
+
+	// implicit typing
+	var anotherString = "This is still Go!"
+	fmt.Println(anotherString)
+	fmt.Printf("The variable's type is %T\n", anotherString)
+
+	// implicit typing, avoid var syntax
+	myString := "Alternative assignment syntax" // this syntax only works for vars inside functions
+	fmt.Println(myString)
+	fmt.Printf("The variable's type is %T\n", myString)
+
+	fmt.Println(cInt)
 }

94546a8caad46ef77e811beeea54f201b0184953
Daniel Kapulkin <dkapulkin@zushealth.com>
2022-04-11 10:30:03 -0400

    practicing props and styling

--- Learning React/hello-react/src/index.js
+++ Learning React/hello-react/src/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// instead of passing in props object, could just make these regular params
+// which would remove the need for "props." syntasx
+function Hello(props) {
+  console.log(Object.keys(props));
+  return (
+    <div>
+      <h1>Welcome to {props.library}!</h1>
+      <p>{props.message}</p>
+      <p>{Object.keys(props).length} Props Total</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  React.createElement("h1", { style: { color: "red" } }, "Hello!"),
+  <Hello library="React" message="Have fun!" number={3} />,
   document.getElementById("root")
 );
